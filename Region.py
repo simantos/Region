ea6edf8e9aa0c2a70173afe6c7ceb1aeadfebdce
simantos <simant@me.com>
2020-05-31 16:50:31 +0200

    added % format

--- Region.py
+++ Region.py
@@ -12,10 +12,14 @@ data=download.drop(columns=["lat","long","codice_regione",
       "isolamento_domiciliare","variazione_totale_positivi","tamponi",
       "casi_testati","nuovi_positivi","totale_positivi","note_it","note_en","stato","data"])
 
-data=data.rename(columns={"denominazione_regione" : "regione",
-    "dimessi_guariti":"guariti"})
 
+data["ratio"]=data["deceduti"].tail(21)/data["totale_casi"].tail(21)*100
+format_dict = {'ratio': '{:.2%}'}
+(data
+ .style
+ .format(format_dict)
+ .hide_index()
+ .highlight_max(color='lightgreen')
+ .highlight_min(color='#cd4f39'))
 
-data["ratio"]=data["deceduti"].tail(21)/data["totale_casi"].tail(21)
-#print(data["deceduti"].tail(21)/data["totale_casi"].tail(21))
 print(data.tail(21))
